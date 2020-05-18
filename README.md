be4bbb0ed5785f4af480ec6e4911809027844542
SGKoishi <i@sgkoi.dev>
2020-05-18 00:00:30 -0700

    Update tool

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Terraria Translations
 Unofficial Terraria 1.4 Chinese localization.
-* `converter`: Tool to convert between gettext style (`.PO`) and Terraria style (`.json`) localization file.
+* `converter`: Tool to extract localizations, and convert between gettext style (`.PO`) and Terraria style (`.json`) localization file.
 * `legacy`: Legacy Terraria 1.3 localization files.
 * `source`: Terraria 1.4 localization files.
 * `project`: OmegaT project
+
+### Usage
+1 Compile `Chireiden.Terraria.sln`
+2 Run `Chireiden.Terraria.Localization extract path\to\Terraria.exe sourceLanguage targetLanguage`
+  (`de-DE`, `en-US`, `es-ES`, `fr-FR`, `it-IT`, `pl-PL`, `pt-BR`, `ru-RU`, `zh-Hans`)
+3 Copy the `output.po` created by converter to `project\source`
+4 Open OmegaT, start your translation
+5 After you done translation, run `Chireiden.Terraria.Localization repack path\to\Terraria.exe targetLanguage path\to\po`
+  (Target po file usually locate in `project\target`)
+6 Now you should get a patched version of Terraria! It will be in the same folder as `Terraria.exe` but called `Terraria_locpatched.exe`
---
+++ tool/Program.cs
@@ -0,0 +1,245 @@
+﻿using dnlib.DotNet;
+using Karambolo.PO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Chireiden.Terraria.Converter
+{
+    internal static class Program
+    {
+        private static void Usage()
+        {
+            Console.WriteLine(@"Usage:
+    extract - extract .json and .po file from Terraria
+        [filePath] - path of Terraria.exe
+        [languages] - optional
+    repack - import .po file to Terraria
+        [filePath] - path of Terraria.exe
+        [language] - language to import
+        [files] - path of .po file");
+            Console.ReadLine();
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Usage();
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "extract":
+                    ExtractCommand(args);
+                    return;
+                case "convert":
+                    ConvertCommand(args);
+                    return;
+                case "repack":
+                    RepackCommand(args);
+                    return;
+            }
+        }
+
+        private static void ExtractCommand(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Usage();
+                return;
+            }
+
+            var filePath = args[1];
+            var languages = args.Skip(2).ToList();
+            var langFileDict = new List<(string Language, string FileName)>();
+            var asm = AssemblyDef.Load(filePath);
+            foreach (var item in asm.ManifestModule.Resources)
+            {
+                if (item.Name.StartsWith("Terraria.Localization.Content"))
+                {
+                    var matched = languages.Where(l => item.Name.Contains(l));
+                    if (languages.Count == 0)
+                    {
+                        File.WriteAllBytes(item.Name, (item as EmbeddedResource)?.CreateReader().ReadRemainingBytes() ?? new byte[0]);
+                    }
+                    else if (matched.Any())
+                    {
+                        langFileDict.Add((matched.Single(), item.Name));
+                        File.WriteAllBytes(item.Name, (item as EmbeddedResource)?.CreateReader().ReadRemainingBytes() ?? new byte[0]);
+                    }
+                }
+            }
+
+            var list = new List<Element>();
+
+            if (languages.Count == 2)
+            {
+                foreach (var item in langFileDict)
+                {
+                    foreach (var node in JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(item.FileName)))
+                    {
+                        foreach (var kvp in node.Value)
+                        {
+                            var fileName = Path.GetExtension(Path.GetFileNameWithoutExtension(item.FileName)).Trim('.');
+                            if (fileName == item.Language)
+                            {
+                                fileName = "Main";
+                            }
+
+                            list.MergeEntry(item.Language == languages[0] ? new Element
+                            {
+                                FileName = fileName,
+                                NodeName = node.Key,
+                                KeyName = kvp.Key,
+                                Source = kvp.Value
+                            } : new Element
+                            {
+                                FileName = fileName,
+                                NodeName = node.Key,
+                                KeyName = kvp.Key,
+                                Target = kvp.Value
+                            });
+                        }
+                    }
+                }
+            }
+
+            var catalog = new POCatalog
+            {
+                Encoding = "UTF-8"
+            };
+
+            foreach (var item in list)
+            {
+                catalog.AddEntry(item.Source, $"{item.FileName}.{item.NodeName}.{item.KeyName}", item.Target);
+            }
+
+            new POGenerator().Generate(File.OpenWrite("output.po"), catalog);
+        }
+
+        private static void ConvertCommand(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RepackCommand(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Usage();
+                return;
+            }
+
+            var filePath = args[1];
+            var locFiles = args.Skip(2).ToList();
+            var lang = locFiles[0];
+            var pofile = locFiles[1];
+
+            var list = new List<Element>();
+            var parseResult = new POParser().Parse(File.OpenRead(pofile));
+            var catalog = parseResult.Catalog;
+            foreach (var item in catalog)
+            {
+                var context = item.Key.ContextId.Split('.');
+                list.Add(new Element
+                {
+                    FileName = context[0],
+                    NodeName = context[1],
+                    KeyName = context[2],
+                    Target = catalog.GetTranslation(item.Key)
+                });
+            }
+
+            var result = new Dictionary<string, Dictionary<string, Dictionary<string, string>>>();
+            foreach (var fileGroup in list.GroupBy(i => i.FileName))
+            {
+                result[fileGroup.Key] = new Dictionary<string, Dictionary<string, string>>();
+                foreach (var node in fileGroup.GroupBy(i => i.NodeName))
+                {
+                    result[fileGroup.Key][node.Key] = node.ToDictionary(i => i.KeyName, i => i.Target);
+                }
+            }
+
+            var asm = AssemblyDef.Load(filePath);
+            foreach (var item in result)
+            {
+                var fileName = "." + item.Key;
+                if (fileName == ".Main")
+                {
+                    fileName = "";
+                }
+                var res = asm.ManifestModule.Resources.Where(r => r.Name.Contains("zh-Hans" + fileName + ".json"));
+                if (!res.Any() && string.IsNullOrEmpty(fileName))
+                {
+                    fileName = ".Main";
+                }
+                try
+                {
+                    var single = res.Single();
+                    asm.ManifestModule.Resources.Remove(single);
+                    asm.ManifestModule.Resources.Add(new EmbeddedResource($"Terraria.Localization.Content.zh-Hans{fileName}.json", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(item.Value, Formatting.Indented)), ManifestResourceAttributes.Public));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{fileName} fail: {e}");
+                }
+            }
+
+            asm.Write(Path.Combine(Path.GetDirectoryName(filePath), "Terraria_locpatched.exe"));
+        }
+    }
+
+    public static class Helper
+    {
+        public static void AddEntry(this POCatalog catalog, string source, string context, string translation)
+        {
+            if (source == "")
+            {
+                return;
+            }
+
+            catalog.Add(new POSingularEntry(new POKey(source, contextId: context))
+            {
+                Translation = translation,
+                Comments = new List<POComment>()
+            });
+        }
+
+        public static void MergeEntry(this List<Element> list, Element element)
+        {
+            foreach (var item in list)
+            {
+                if (item.FileName == element.FileName && item.NodeName == element.NodeName && item.KeyName == element.KeyName)
+                {
+                    if (!string.IsNullOrWhiteSpace(element.Source))
+                    {
+                        item.Source = element.Source;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(element.Target))
+                    {
+                        item.Target = element.Target;
+                    }
+
+                    break;
+                }
+            }
+
+            list.Add(element);
+        }
+    }
+
+    public class Element
+    {
+        public string FileName = "";
+        public string NodeName = "";
+        public string KeyName = "";
+        public string Source = "";
+        public string Target = "";
+    }
+}
